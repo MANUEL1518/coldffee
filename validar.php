d69ec28e28ace5c60880e3550c523805f0832160
loquendo_118_loco@hotmail.com <loquendo_118_loco@hotmail.com>
2019-04-19 14:40:23 -0500

    ya tiene validar.php, mofifico diseño de verify

---
+++ validar.php
@@ -0,0 +1,23 @@
+<?php 
+	include("med/datos_serv.php");
+	$email = $_POST['email'];
+	$password = $_POST['pass'];
+
+	$conexion = mysqli_connect($host, $user, $pass, $db);
+	if ($conexion) {
+		$query = mysqli_query($conexion, "SELECT * FROM users WHERE email = '".$email."';");
+		if (mysqli_num_rows($query) > 0) {
+			while ($row = mysqli_fetch_array($query)) {
+				if ($row['password'] == $password) {
+					echo "Bienvenido";
+				}else{
+					echo "Contraseña invalida :P";
+				}
+			}
+		}else{
+			echo "Correo o contraeña incorrecto";
+		}
+	}else{
+		echo "problema al realizar conexion";
+	}
+?>
