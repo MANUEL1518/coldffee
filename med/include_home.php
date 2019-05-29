37e57fac59b9352bc6fa42a24162f7986ae72814
MANUEL1518 <loquendo_118_loco@hotmail.com>
2019-05-29 17:11:40 -0500

    Muestra de archivos

--- med/include_home.php
+++ med/include_home.php
@@ -3,34 +3,55 @@ function content_main(){
 	$conexion = new mysqli("localhost", "root", "password", "coldffee");
 	if ($conexion) {
 		$query = $conexion->query("SELECT * FROM users");
+		$files = $conexion->query("SELECT * FROM files WHERE id_user = '".$id_user."'");
+		$condicion = $files->num_rows;
 		if($query){
 			?>
 
 <div id="cont_main_home">
 	<table border="0">
-		<tr>
-			<th colspan="5">
-				Archivos pasados
-			</th>
-		</tr>
-		<tr>
-			<td></td>
-			<td>Nombres</td>
-			<td>Extension</td>
-			<td>Size</td>
-			<td>Fecha de subida</td>
-		</tr>
+		<form>
 		<tr>
 			<td>
-				<form>
-					<input type="checkbox" name="file_1">
-				</form>
+				<input type="checkbox">
 			</td>
-			<td>Nombre</td>
-			<td>png</td>
-			<td>120.5MB</td>
-			<td>2019-12-04</td>
+			<td colspan="4">
+				<div id="btn_icon_event" title="Upload">
+					<div class="icon_upload"></div>
+				</div>
+				<div id="btn_icon_event" title="Download">
+					<div class="icon_download"></div>
+				</div>
+				<div id="btn_icon_event" title="Delete">
+					<div class="icon_delete"></div>
+				</div>
+			</td>
 		</tr>
+		<?php
+			if ($condicion > 0) {
+		?>
+		<tr class="tr_title">
+			<td></td>
+			<td>Name</td>
+			<td>Extension</td>
+			<td>Size</td>
+			<td>Date Uploaded</td>
+		</tr>
+			<tr>
+				<td>
+					<input type="checkbox">
+				</td>
+				<td>Nombre</td>
+				<td>png</td>
+				<td>120.5MB</td>
+				<td>2019-12-04</td>
+			</tr>
+		<?php
+			}else{
+				echo "<tr><td colspan='5'><center><h2 style='padding: 20px 0px;'>No hay archivos subidos aun</h2></center></td></tr>";
+			}
+		?>
+		</form>
 	</table>
 </div>
 
